fc600925a8791b956f6579dacc3df362b9067caf
Amit Arbel <amit.arbel@microfocus.com>
2019-03-15 15:32:20 +0200

    Devices service > Adding new device using server

--- src/app/common/consts.ts
+++ src/app/common/consts.ts
@@ -1,21 +1,21 @@
 export const DeviceTypes = {
-  workstation: "workstation",
-  server: "server",
-  mobile: "mobile",
+  workstation: 'workstation',
+  server: 'server',
+  mobile: 'mobile',
 };
 
 export const DeviceStatuses = {
-  active: "active",
-  inactive: "inactive",
+  active: 'active',
+  inactive: 'inactive',
 };
 
 export const EventLogEntryTypes = {
-  security: "security",
-  log: "log",
+  security: 'security',
+  log: 'log',
 };
 
 export const EventLogEntrySeverities = {
-  high: "high",
-  medium: "medium",
-  low: "low",
+  high: 'high',
+  medium: 'medium',
+  low: 'low',
 };
--- src/app/services/rest/devices-rest.service.ts
+++ src/app/services/rest/devices-rest.service.ts
@@ -1,43 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import {
-  AddDeviceResponse, DeviceType, GetDevicesResponse, IDevice, IEventLogEntry,
-  INewDeviceParams
-} from '../../common/interfaces';
-import {DeviceStatuses, DeviceTypes, EventLogEntrySeverities, EventLogEntryTypes} from '../../common/consts';
-import {HttpClient} from "@angular/common/http";
-
-const DeviceBrandsByType = {
-  [DeviceTypes.mobile]: {
-    Samsung: 0,
-    HTC: 0,
-    Apple: 0,
-    LG: 0,
-  },
-  [DeviceTypes.server]: {
-    HP: 0,
-    Dell: 0,
-    IBM: 0,
-    Cisco: 0,
-    Lenovo: 0,
-  },
-  [DeviceTypes.workstation]: {
-    HP: 0,
-    Dell: 0,
-    Lenovo: 0,
-    Apple: 0,
-    Samsung: 0,
-    Asus: 0,
-  }
-};
-
-const descriptions = [
-  'something bad happend',
-  'user had logged in',
-  'user\'s password is weak',
-  'user had logged out',
-  'application had crashed for unknown reason',
-];
+import { AddDeviceResponse, GetDevicesResponse, INewDeviceParams } from '../../common/interfaces';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -63,97 +27,11 @@ export class DevicesRestService {
   }
 
   public addDevice(params: INewDeviceParams): Observable<AddDeviceResponse> {
-    const newDeviceObservable = new Observable<AddDeviceResponse>(observer => {
-      setTimeout(() => {
-        const newDevice = this.generateDevice(params);
-
-        observer.next(newDevice);
-        observer.complete();
-
-      }, 1000);
-    });
-
-    return newDeviceObservable;
+    const url = '/device';
+    return this.httpClient.post<AddDeviceResponse>(this.buildFullURL(url), params);
   }
 
   private buildFullURL(url) {
     return `${this.baseURL}${url}`;
-  }
-
-  private generateDevice(params?: INewDeviceParams): IDevice {
-    params = params || {} as INewDeviceParams;
-
-    const deviceType = this.getRandomValue(DeviceTypes);
-
-    const newDevice: IDevice = {
-      name: params.name || this.generateName(deviceType),
-      ipAddress: params.ip || this.generateIP(),
-      type: deviceType,
-      status: this.getRandomValue(DeviceStatuses),
-      events: this.generateEvents(),
-    };
-
-    return newDevice;
-  }
-
-  private generateIP(): string {
-    const ip = [];
-    for (let i = 0; i < 4; i++) {
-      ip.push(Math.floor(127 * Math.random()));
-    }
-    return ip.join('.');
-  }
-
-  private generateName(ofType: DeviceType) {
-    const deviceBrands = DeviceBrandsByType[ofType];
-    const deviceBrand = this.getRandomValue(deviceBrands, true);
-    const deviceBrandCounter = ++deviceBrands[deviceBrand];
-
-    const devicePostfix =
-      ofType === DeviceTypes.mobile && 'device' ||
-      ofType === DeviceTypes.server && 'machine' || '';
-    const deviceDescription = [ofType, devicePostfix].join(' ');
-    const name = `${deviceBrand} ${deviceDescription} #${deviceBrandCounter}`;
-
-    return name;
-  }
-
-
-  generateEvents(tail: number = 5): IEventLogEntry[] {
-    const events = [];
-
-    for (let i = 0; i < tail; i++) {
-      events.push(this.generateEvent());
-    }
-
-    return events;
-  }
-
-  private generateEvent(): IEventLogEntry {
-
-    const severity = this.getRandomValue(EventLogEntrySeverities);
-    const type = this.getRandomValue(EventLogEntryTypes);
-    const description = this.getRandomValue(descriptions);
-
-    const event = {
-      type,
-      severity,
-      description,
-    };
-
-    return event;
-  }
-
-  private getRandomValue(ofObject: object, returnKey: boolean = false) {
-    const objectKeys = Object.keys(ofObject);
-    const randomKeyIndex = Math.floor(objectKeys.length * Math.random());
-
-    const randomKey = objectKeys[randomKeyIndex];
-    if (returnKey) {
-      return randomKey;
-    }
-
-    const randomValue = ofObject[randomKey];
-    return randomValue;
   }
 }
