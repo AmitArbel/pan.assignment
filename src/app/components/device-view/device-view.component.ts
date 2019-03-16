c99532962498081da1675531806f16f08eea1fde
Amit Arbel <amit.arbel@microfocus.com>
2019-03-17 00:49:25 +0200

    Devices view > a little styling

--- src/app/components/device-view/device-view.component.ts
+++ src/app/components/device-view/device-view.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {IDevice} from '../../common/interfaces';
+import {DeviceStatuses, EventLogEntrySeverities, EventLogEntryTypes} from '../../common/consts';
 
 @Component({
   selector: 'app-device-view',
@@ -7,8 +8,11 @@ import {IDevice} from '../../common/interfaces';
   styleUrls: ['./device-view.component.scss']
 })
 export class DeviceViewComponent implements OnInit {
-
   @Input() device: IDevice;
+
+  deviceStatuses = DeviceStatuses;
+  eventSeverities = EventLogEntrySeverities;
+  eventTypes = EventLogEntryTypes;
 
   constructor() { }
 
--- src/app/components/devices-view/devices-view.component.ts
+++ src/app/components/devices-view/devices-view.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DevicesService } from 'src/app/services/devices.service';
-import { Observable } from 'rxjs';
 import {GetDevicesResponse} from '../../common/interfaces';
-import {EventLogEntrySeverities, EventLogEntryTypes} from '../../common/consts';
-import {ToastrHelper} from "../../common/helpers/toastr-helper";
+import {ToastrHelper} from '../../common/helpers/toastr-helper';
 
 @Component({
   selector: 'app-devices-view',
@@ -14,9 +12,6 @@ import {ToastrHelper} from "../../common/helpers/toastr-helper";
 export class DevicesViewComponent implements OnInit {
 
   allDevices: GetDevicesResponse;
-
-  eventSeverities = EventLogEntrySeverities;
-  eventTypes = EventLogEntryTypes;
 
   constructor(private devicesSvc: DevicesService) {
     let newDeviceObservableSubscription;
